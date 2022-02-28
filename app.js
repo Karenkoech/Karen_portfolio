f9dea67e2d69b7764ab164c6a028936ce8676084
Karenkoech <karenkoech3@gmail.com>
2022-02-28 02:30:00 -0500

    Add changes made to files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const menu=document.querySelector('#mobile-menu');
+/*const menu=document.querySelector('#mobile-menu');
 const menulinks=document.querySelector('.navbar__menu');
 
 menu.addEventListener('click',function(){
